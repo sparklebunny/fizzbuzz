d660b605b11284f0ac9bd93a6088b4deb26a8b9a
Sparklebunny Stevens <jeribeth@gmail.com>
2014-04-02 21:15:17 -0600

    refactored fizz buzz
    refactor of fizz buzz

--- app.js
+++ app.js
@@ -1,31 +1,40 @@
 $(document).ready(function(){
 
 
-$("#enterButton").click(function(button) {
+	$("#enterButton").click(function(button) {
 
-	var inputValue = $("#myNumber").val();
+		var inputValue = +$("#myNumber").val();
+		$("#display").html("");
 
-	$("#display").html("");
 
-	for (var i = 1; i <= inputValue; i++) {
+		for (var i = 1; i <= inputValue; i++) {
+			var result = checkNumber(i);
+			$("#display").append("<p>" + result + "</p>");
 
-		if ((i % 5 == 0) && (i % 3 == 0)) {
-			$("#display").append("<p>FizzBuzz</p>");
-		} else if (i % 5 == 0) {
-			$("#display").append("<p>Buzz</p>");
-		} else if (i % 3 == 0) {
-			$("#display").append("<p>Fizz</p>");
-		} else {
-			$("#display").append("<p>" + i + "</p>");
+		}	
+	});
+
+
+
+	$("#myNumber").text(function() {
+		if($("#myNumber").val() == " "); {
+			alert("Enter a number from 1 - 100");
 		}
+	});
 
+	var checkNumber = function (number) {
+
+		if (number % 15 == 0) {
+			return "FizzBuzz";
+		} else if (number % 5 == 0) {
+			return "Buzz";
+		} else if (number % 3 == 0) {
+			return "Fizz";
+		} else {
+			return number;
+		}
 	}
-});
 
 });
-
-// replace console.log to append output to div
-
-
 
 
